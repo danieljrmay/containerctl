de4de535e41f4f15bb98b877f038f3da43c0bf29
Daniel J. R. May <daniel.may@kada-media.com>
2022-06-28 13:59:02 +0100

    Update for drupal 9.4.1

--- src/usr/local/share/containerctl/drupal/9/environment.inc.bash
+++ src/usr/local/share/containerctl/drupal/9/environment.inc.bash
@@ -1,5 +1,5 @@
 # Drupal 9 environment variables and their default values
 
-: "${DRUPAL9_VERSION:=9.3.14}"
-: "${HOST_CUSTOM_MODULES_DIR:=$(realpath ~/Projects/Drupal/Modules)}"
-: "${HOST_CUSTOM_THEMES_DIR:=$(realpath ~/Projects/Drupal/Themes)}"
+: "${DRUPAL9_VERSION:=9.4.1}"
+: "${HOST_CUSTOM_MODULES_DIR:=$(realpath ~/Projects/Drupal/9/Modules)}"
+: "${HOST_CUSTOM_THEMES_DIR:=$(realpath ~/Projects/Drupal/9/Themes)}"
